a76dea4f52dc07fe2d4e1fdf2e7d8ea6f5331bc0
xszefo <piotr.sumelka@gmail.com>
2022-04-14 16:54:49 +0200

    minor detail

--- string_manipulation.sh
+++ string_manipulation.sh
@@ -13,7 +13,7 @@ echo "Uppercase str1: ${str1^^}"
 echo "Uppercase str2: ${str2^^}"
 
 echo "Lowercase str1 for aeiou: ${str1,,[aeiou]}"
-echo "LOwercase str2 for aeiou: ${str2,,[aeiou]}"
+echo "Lowercase str2 for aeiou: ${str2,,[aeiou]}"
 
 echo "Uppercase str1 for aeiou: ${str1^^[aeiou]}"
 echo "Uppercase str2 for aeiou: ${str2^^[aeiou]}"
